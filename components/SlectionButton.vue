bed34a8940a1f8fdd400eb9a3c188d4c9583622f
erichier <developer.erichier@gmail.com>
2020-03-21 21:28:30 +0100

    Renamed button, added all font sizes

---
+++ components/SlectionButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button v-on:click="sendclick" class="">
+    <slot/>
+  </button>
+</template>
+<script>
+  export default  {
+    methods: {
+      sendclick() {
+        this.$emit("click");
+      }
+    }
+  }
+</script>
