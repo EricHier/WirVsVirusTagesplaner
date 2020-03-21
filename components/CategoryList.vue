ee6bd56ade417f11fba35e5796c6ecb221d64f32
Anna Ira <annairahurnaus@mytum.de>
2020-03-21 20:27:49 +0100

    add id to categories

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -4,32 +4,32 @@
       <CategoryItem
         v-for="item in list"
         v-bind:item="item"
-        v-bind:key="item.name"
+        v-bind:key="item.id"
       >
       </CategoryItem>
     </ul>
   </div>
 </template>
 <script>
-import CategoryItem from '~/components/CategoryItem.vue';
+  import CategoryItem from '~/components/CategoryItem.vue';
 
-export default {
-  components: {
-    CategoryItem
-  },
-  data() {
-    return {
-      list: [
-        { name: 'Soziale Kontakte', checked: false },
-        { name: 'Bewegung', checked: false },
-        { name: 'Ernährung', checked: false },
-        { name: 'Schlaf', checked: false },
-        { name: 'Positiver Fokus', checked: false },
-        { name: 'Kreatives', checked: false },
-        { name: 'Entspannung', checked: false },
-        { name: 'Produktiv sein', checked: false },
-      ],
+  export default {
+    components: {
+      CategoryItem
+    },
+    data() {
+      return {
+        list: [
+          {"id": 1, name: 'Soziale Kontakte', checked: false},
+          {"id": 2, name: 'Bewegung', checked: false},
+          {"id": 3, name: 'Ernährung', checked: false},
+          {"id": 4, name: 'Schlaf', checked: false},
+          {"id": 5, name: 'Positiver Fokus', checked: false},
+          {"id": 6, name: 'Kreatives', checked: false},
+          {"id": 7, name: 'Entspannung', checked: false},
+          {"id": 8, name: 'Produktiv sein', checked: false},
+        ],
+      }
     }
   }
-}
 </script>
