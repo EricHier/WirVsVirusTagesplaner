78028f887dfad2ab1d9771101b6b7dd887572c96
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-21 18:00:14 +0100

    Set up a simple list

---
+++ components/plan/ListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ul>
+      <li>{{data.name}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['data'],
+}
+</script>
--- pages/planner.vue
+++ pages/planner.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <h1>Plan your day</h1>
+    <ListItem v-for="item in list" v-bind:data="item"></ListItem>
   </div>
 </template>
 <script>
+import ListItem from '~/components/plan/ListItem.vue'
+
+export default {
+  components: {
+    ListItem
+  },
+  data() {
+    return {
+      list: [
+        {name: 'Arbeiten', check: false},
+        {name: 'Sport', check: true}
+      ],
+    }
+  }
+}
 </script>
