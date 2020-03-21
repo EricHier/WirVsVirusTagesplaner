ddcecdf1acd6e6666ebca9046d096458d7f0be8e
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-21 19:50:34 +0100

    CategoryList component

---
+++ components/CategoryList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <ListItem v-for="item in list" v-bind:data="item"></ListItem>
+    </ul>
+  </div>
+</template>
+<script>
+import ListItem from '~/components/plan/ListItem.vue';
+
+export default {
+  components: {
+    ListItem
+  },
+  data() {
+    return {
+      list: [
+        { name: 'Soziale Kontakte', checked: false },
+        { name: 'Bewegung', checked: false },
+        { name: 'Ernährung', checked: false },
+        { name: 'Schlaf', checked: false },
+        { name: 'Positiver Fokus', checked: false },
+        { name: 'Kreatives', checked: false },
+        { name: 'Entspannung', checked: false },
+        { name: 'Produktiv sein', checked: false },
+      ],
+    }
+  }
+}
+</script>
--- pages/planner.vue
+++ pages/planner.vue
@@ -1,31 +1,15 @@
 <template>
   <div>
     <h1>Was hast du morgen vor?</h1>
-    <ul>
-      <ListItem v-for="item in list" v-bind:data="item"></ListItem>
-    </ul>
+    <CategoryList></CategoryList>
   </div>
 </template>
 <script>
-import ListItem from '~/components/plan/ListItem.vue'
+import CategoryList from '~/components/CategoryList.vue'
 
 export default {
   components: {
-    ListItem
-  },
-  data() {
-    return {
-      list: [
-        { name: 'Soziale Kontakte', checked: false },
-        { name: 'Bewegung', checked: false },
-        { name: 'Ernährung', checked: false },
-        { name: 'Schlaf', checked: false },
-        { name: 'Positiver Fokus', checked: false },
-        { name: 'Kreatives', checked: false },
-        { name: 'Entspannung', checked: false },
-        { name: 'Produktiv sein', checked: false },
-      ],
-    }
+    CategoryList
   }
 }
 </script>
