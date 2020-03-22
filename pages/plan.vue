0c9062d8aa4ee4e288f8079e041dee778edc3352
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 11:32:46 +0100

    Rewrite plan with CategoryTimeSlot component

--- pages/plan.vue
+++ pages/plan.vue
@@ -1,40 +1,44 @@
 <template>
   <div class="container p-8 background">
     <h1 class="title my-8">Dein Plan für heute</h1>
-    <div>
-      <h2>Morgens</h2>
-      <p v-if="selectedCategories[0]">{{selectedCategories[0].name}}</p>
-      <p v-else>Nicht ausgewählt.</p>
-    </div>
-    <div>
-      <h2>Mittags</h2>
-      <p v-if="selectedCategories[1]">{{selectedCategories[1].name}}</p>
-      <p v-else>Nicht ausgewählt.</p>
-    </div>
-    <div>
-      <h2>Abends</h2>
-      <p v-if="selectedCategories[2]">{{selectedCategories[2].name}}</p>
-      <p v-else>Nicht ausgewählt.</p>
-    </div>
+    <CategoryTimeSlot
+      v-for="item in timeSlots"
+      v-bind:item="item"
+      v-bind:key="item.id"
+      v-on:click="onClick">
+    </CategoryTimeSlot>
   </div>
 </template>
 <script>
+import CategoryTimeSlot from '~/components/CategoryTimeSlot.vue';
 import { categories } from '~/data/categories';
 
 export default {
+  components: {CategoryTimeSlot},
+  methods: {
+    onClick(e) {
+      console.log("click", e);
+    }
+  },
   data() {
     return {
-      selectedCategories: []
+      timeSlots: []
     }
   },
   created() {
     const categoryIds = this.$route.query?.categoryIds || [];
-    this.selectedCategories = categoryIds.map(id => categories[id]);
+    const selectedCategories = categoryIds.map(id => categories[id]);
+    const defaultTimeSlots = [
+      {id: 0, name: 'Morgens', category: selectedCategories[0]},
+      {id: 1, name: 'Mittags', category: selectedCategories[1]},
+      {id: 2, name: 'Abends', category: selectedCategories[2]}
+    ];
+    this.timeSlots = defaultTimeSlots;
   }
 }
 </script>
 <style>
-  .header {
-    display: flex;
-  }
+.header {
+  display: flex;
+}
 </style>
