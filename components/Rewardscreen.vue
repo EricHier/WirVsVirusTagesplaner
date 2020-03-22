1dc1665ca81e0ad08311f11bcff1fb24097ffba9
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 20:43:42 +0100

    Reward screen

---
+++ components/Rewardscreen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container absolute top-0">
+    <h1>Yeah.</h1>
+    <p class="nunito">Sei stolz auf dich!</p>
+  </div>
+</template>
+<style>
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  h1 {
+    font-size: 58px;
+  }
+</style>
