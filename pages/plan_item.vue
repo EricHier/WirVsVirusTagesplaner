6b81547817ddd315263cb44c9f959f0ead7c33b2
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 12:01:37 +0100

    Read category details from URL

--- pages/plan_item.vue
+++ pages/plan_item.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <h1>Plan Details</h1>
+    <p>{{categoryDetails}}</p>
   </div>
 </template>
 <script>
+import { category_details } from '~/data/category_details';
+
+export default {
+  components: {},
+  methods: {
+  },
+  data() {
+    return {
+      categoryDetails: {}
+    }
+  },
+  created() {
+    const categoryId = this.$route.query.categoryId;
+
+    for(const details of category_details) {
+      if(details.category_id == categoryId) {
+        this.categoryDetails = details;
+      }
+    }
+  }
+}
 </script>
