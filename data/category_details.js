023ae2447d6743538e4e3b11ee5caef480833e0f
Anna Ira <annairahurnaus@mytum.de>
2020-03-22 11:00:25 +0100

    make it a js const and add category details Bewegung

---
+++ data/category_details.js
@@ -0,0 +1,74 @@
+export const category_details = [
+  {
+    "name": "Kontakt zu Anderen",
+    "category_id": 1,
+    "activities": [
+      {
+        "name": "Einen Freund Anrufen",
+        "image": "images/call.png",
+        "text": "Ruf doch mal wieder einen alten Freund an."
+      },
+      {
+        "name": "Mit der Familie Telefonieren",
+        "image": "images/group_call.png",
+        "text": "Deine Verwandte freuen sich, von dir zu hören."
+      },
+      {
+        "name": "Einen Brief schreiben",
+        "image": "images/letter.png",
+        "text": "Warum nicht mal wieder analog?"
+      },
+      {
+        "name": "Via Skype gemeinsam Essen",
+        "image": "images/skype.png",
+        "text": "Gemeinsames Essen ist auch digital möglich."
+      }
+    ]
+  },
+  {
+    "name": "Bewegung",
+    "category_id": 2,
+    "activities": [
+      {
+        "name": "Yoga",
+        // "image": "images/placeholder.png",
+        "link": "https://www.youtube.com/channel/UCHJBoCDxaCTRrwCHXEBA-BA"
+      },
+      {
+        "name": "Spazieren gehen",
+      },
+      {
+        "name": "Fitness",
+        "link": "https://www.freeletics.com/",
+      },
+      {
+        "name": "Joggen",
+      },
+      {
+        "name": "Wandern",
+      },
+      {
+        "name": "Fahrrad fahren",
+      },
+      {
+        "name": "Achtsames Gehen",
+        "link": "https://dfme-achtsamkeit.de/gehmeditation-achtsam-gehen/",
+      },
+      {
+        "name": "5 Tibeter",
+        "link": "https://www.youtube.com/watch?v=8zdSghvfqrE",
+      },
+      {
+        "name": "Heimgolf",
+        "link": "https://www.feiern1.de/partyspiele/gruppenspiele/socken-golf-s620.html",
+      },
+      {
+        "name": "Tanzen",
+      },
+      {
+        "name": "Sportspiele für drinnen",
+        "link": "https://www.youtube.com/watch?v=PKPQ0M5RRto",
+      },
+    ]
+  }
+];
