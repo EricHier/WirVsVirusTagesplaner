132cee3096bbd0cfb79688e745f8a8e117ff4dbe
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 09:55:40 +0100

    Index array with 0 first

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -39,14 +39,14 @@ export default {
   data() {
     return {
       list: [
-        {"id": 1, name: 'Soziale Kontakte', checked: false},
-        {"id": 2, name: 'Bewegung', checked: false},
-        {"id": 3, name: 'Ernährung', checked: false},
-        {"id": 4, name: 'Schlaf', checked: false},
-        {"id": 5, name: 'Positiver Fokus', checked: false},
-        {"id": 6, name: 'Kreatives', checked: false},
-        {"id": 7, name: 'Entspannung', checked: false},
-        {"id": 8, name: 'Produktiv sein', checked: false},
+        {id: 0, name: 'Soziale Kontakte', checked: false},
+        {id: 1, name: 'Bewegung', checked: false},
+        {id: 2, name: 'Ernährung', checked: false},
+        {id: 3, name: 'Schlaf', checked: false},
+        {id: 4, name: 'Positiver Fokus', checked: false},
+        {id: 5, name: 'Kreatives', checked: false},
+        {id: 6, name: 'Entspannung', checked: false},
+        {id: 7, name: 'Produktiv sein', checked: false},
       ],
     }
   }
