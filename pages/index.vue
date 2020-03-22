20353dde4fa1d04dd9c961a006f61d62732d436b
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 20:55:45 +0100

    Adds reward

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,15 @@
     <CheckinMood v-if="screenIndex === 1" v-on:data="newMoodData" v-bind:inputValue="checkinMood"> </CheckinMood>
     <CheckinPositive v-if="screenIndex === 2" v-on:data="newPositiveData" v-bind:data="checkinPositive" v-on:show="weiterButton = true"></CheckinPositive>
     <CheckinNegative v-if="screenIndex === 3" v-on:data="newNegativeData" v-bind:data="checkinNegative" v-on:show="weiterButton = true"></CheckinNegative>
+    <Rewardscreen v-if="screenIndex === 4"></Rewardscreen>
 
-    <WeiterButton v-if="screenIndex >= 1" v-bind:disabled="!weiterButton" v-bind:back="screenIndex >=2" v-on:back="screenIndex--" v-bind:seethrough="screenIndex >= 2" v-on:click="onClick"></WeiterButton>
+    <WeiterButton v-if="screenIndex >= 1 && screenIndex < 4" v-bind:disabled="!weiterButton" v-bind:back="screenIndex >=2" v-on:back="screenIndex--" v-bind:seethrough="screenIndex >= 2" v-on:click="onClick"></WeiterButton>
   </div>
 </template>
 
 <script>
 import Splashscreen from '~/components/Splashscreen.vue'
+import Rewardscreen from '~/components/Rewardscreen.vue'
 import CheckinMood from '~/components/checkin/CheckinMood.vue'
 import CheckinPositive from '~/components/checkin/CheckinPositive.vue'
 import CheckinNegative from '~/components/checkin/CheckinNegative.vue'
@@ -19,7 +21,7 @@ import WeiterButton from "../components/WeiterButton";
 
 export default {
   components: {
-    WeiterButton, Splashscreen, CheckinMood, CheckinPositive, CheckinNegative
+    WeiterButton, Splashscreen, Rewardscreen, CheckinMood, CheckinPositive, CheckinNegative
   },
   data() {
     return {
@@ -57,10 +59,15 @@ export default {
       }
 
       if (newVal >= 4) {
-        this.$router.push({
-          name: 'planner',
-          query: { categoryIds: /*this.checkinPositive.concat(this.checkinNegative.filter((item) => this.checkinPositive.indexOf(item) < 0))*/ this.checkinNegative }
-        });
+        this.background = "/bg_reward.jpg";
+        let self = this;
+
+        setTimeout(() => {
+          this.$router.push({
+            name: 'planner',
+            query: { categoryIds: this.checkinNegative }
+          });
+        }, 3000);
       }
     }
     // 375 - 812
