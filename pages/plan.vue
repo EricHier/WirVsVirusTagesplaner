298c8d9b1388042040757d5a46bc75ef10315201
Oliver Prater <44027+oliverprater@users.noreply.github.com>
2020-03-22 11:01:41 +0100

    Show category instead of categoryId in plan

--- pages/plan.vue
+++ pages/plan.vue
@@ -3,30 +3,33 @@
     <h1 class="title my-8">Dein Plan für heute</h1>
     <div>
       <h2>Morgens</h2>
-      <p v-if="categoryIds[0]">{{categoryIds[0]}}</p>
-      <p v-else>Nichts</p>
+      <p v-if="selectedCategories[0]">{{selectedCategories[0].name}}</p>
+      <p v-else>Nicht ausgewählt.</p>
     </div>
     <div>
       <h2>Mittags</h2>
-      <p v-if="categoryIds[1]">{{categoryIds[1]}}</p>
-      <p v-else>Nichts</p>
+      <p v-if="selectedCategories[1]">{{selectedCategories[1].name}}</p>
+      <p v-else>Nicht ausgewählt.</p>
     </div>
     <div>
       <h2>Abends</h2>
-      <p v-if="categoryIds[2]">{{categoryIds[2]}}</p>
-      <p v-else>Nichts</p>
+      <p v-if="selectedCategories[2]">{{selectedCategories[2].name}}</p>
+      <p v-else>Nicht ausgewählt.</p>
     </div>
   </div>
 </template>
 <script>
+import { categories } from '~/data/categories';
+
 export default {
   data() {
     return {
-      categoryIds: []
+      selectedCategories: []
     }
   },
   created() {
-    this.categoryIds = this.$route.query?.categoryIds || [];
+    const categoryIds = this.$route.query?.categoryIds || [];
+    this.selectedCategories = categoryIds.map(id => categories[id]);
   }
 }
 </script>
