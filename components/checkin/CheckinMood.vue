f17a93d82e5c15052b8f42ed11495ba9c0ed7029
Simon <srey@inovex.de>
2020-03-22 15:16:48 +0100

    implement checkin mood smileys

--- components/checkin/CheckinMood.vue
+++ components/checkin/CheckinMood.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="w-full">
     <h1 class="w-full my-8 title">Wie geht's dir? </h1>
-
-    <input type="range" min="0" max="4" value="2" class="w-full slider mb-8" v-model="inputValue" id="myRange">
+    <input type="range" min="0" max="4" class="w-full mb-8" v-bind:class="{slidersad: sliderState < 2, slidernormal: sliderState == 2, sliderhappy: sliderState > 2}" v-model="inputValue" id="myRange">
   </div>
 </template>
 <script>
@@ -10,17 +9,23 @@
   import WeiterButton from "../WeiterButton";
 
   export default {
+    data() {
+      return {
+        sliderState: 2
+      }
+    },
     components: {WeiterButton, ButtonDesign},
     props: ["inputValue"],
     watch: {
       inputValue: function (val) {
         this.$emit("data", val);
+        this.sliderState = val;
       }
     }
   }
 </script>
 <style>
-  .slider {
+  .slidersad {
     -webkit-appearance: none;
     appearance: none;
     outline: none;
@@ -29,12 +34,53 @@
     border-radius: 5px;
   }
 
-  .slider::-webkit-slider-thumb {
+  .slidersad::-webkit-slider-thumb {
     -webkit-appearance: none; /* Override default look */
     appearance: none;
-    width: 25px; /* Set a specific slider handle width */
-    height: 25px; /* Slider handle height */
+    width: 100px; /* Set a specific slider handle width */
+    height: 100px; /* Slider handle height */
+    background-image: url("/mood_sad.png");
+    background-size: 100px 100px;
+    cursor: pointer; /* Cursor on hover */
+    border-radius: 50px;
+  }
+
+  .slidernormal {
+    -webkit-appearance: none;
+    appearance: none;
+    outline: none;
+    height: 5px;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .slidernormal::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 100px; /* Set a specific slider handle width */
+    height: 100px; /* Slider handle height */
+    background-image: url("/mood_neutral.png");
+    background-size: 100px 100px;
+    cursor: pointer; /* Cursor on hover */
+    border-radius: 50px;
+  }
+
+  .sliderhappy {
+    -webkit-appearance: none;
+    appearance: none;
+    outline: none;
+    height: 5px;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .sliderhappy::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 100px; /* Set a specific slider handle width */
+    height: 100px; /* Slider handle height */
     background-image: url("/mood_happy.png");
+    background-size: 100px 100px;
     cursor: pointer; /* Cursor on hover */
     border-radius: 50px;
   }
