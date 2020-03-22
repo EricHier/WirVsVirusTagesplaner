0adfdb4d8b05eca7c21dfe61068540f850910f3d
erichier <developer.erichier@gmail.com>
2020-03-22 19:35:18 +0100

    fixed design, set link href to target

--- components/CategoryActivity.vue
+++ components/CategoryActivity.vue
@@ -1,7 +1,7 @@
 <template>
-  <a v-bind:href="item.link" class="flex justify-between block border-2 border-orange mx-12 my-2 p-4 nunito font-bold rounded-sm box">
-    <img src="/link-24px.svg" v-if="typeof item.link !== 'undefined'" width="24px" height="24px" class="mr-4">
-    <h3 class="text-center url-icon">{{item.name}}</h3>
+  <a v-bind:href="item.link" target="_blank" class="block border-2 border-orange text-center mx-12 my-2 p-4 nunito font-bold rounded-sm box">
+    <img src="/link-24px.svg"  v-if="typeof item.link !== 'undefined'" width="24px" height="24px" class="mr-4  inline-block img">
+    <h3 class="url-icon inline-block">{{item.name}}</h3>
   </a>
 </template>
 <script>
@@ -15,5 +15,11 @@ export default {
     color: #FFB115;
   }
 
+  .img {
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    margin-left: 4rem;
+  }
 
 </style>
